47de7b880e91d60c149236a0932ff89d55559856
Shawn Tompke <shawntompke@gmail.com>
2022-11-16 13:24:58 -0600

    BREAKING CHANGE(button): breaking change in button.

--- packages/button/lib/button.js
+++ packages/button/lib/button.js
@@ -3,5 +3,5 @@
 module.exports = button;
 
 function button() {
-    return "Hello from button, and im here too. Bunch of stuff added.";
+    return "Hello from button,      breaking change and im here too. Bunch of stuff added.";
 }
