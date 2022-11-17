78239c459aa9cea883295b825477f0bebbf00747
GitHub Action <action@github.com>
2022-11-17 09:57:19 -0600

    fix(link): Fixed bug and comments.

--- packages/link/lib/link.js
+++ packages/link/lib/link.js
@@ -5,9 +5,6 @@ module.exports = link;
 // comment here
 // comment here
 // comment here
-// comment here
-// comment here
-// comment here
 
 function link() {
     return "Hello from link, this is a fixed a bug and some stuff added some features";
