d674dbca15d968219f8cc03bd45cfeb4b24cff45
Shawn Tompke <30378642+Stompke@users.noreply.github.com>
2022-11-17 09:47:58 -0600

    fix(link): Fixed bug on link (#10)
    * fix(link): Fixed a bug for link.
    
    Co-authored-by: GitHub Action <action@github.com>

--- packages/link/lib/link.js
+++ packages/link/lib/link.js
@@ -6,6 +6,8 @@ module.exports = link;
 // comment here
 // comment here
 // comment here
+// comment here
+// comment here
 
 function link() {
     return "Hello from link, this is a fixed a bug and some stuff";
