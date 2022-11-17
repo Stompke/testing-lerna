d7b3a280975cdbc9400c7f59987a96a17b72b546
GitHub Action <action@github.com>
2022-11-17 13:38:34 -0600

    feat(link): features in link.

--- packages/link/lib/link.js
+++ packages/link/lib/link.js
@@ -7,5 +7,5 @@ module.exports = link;
 // comment here
 
 function link() {
-    return "Hello from link, feature on link this is a fixed a bug and some stuff added some features";
+    return "Hello from link, fstff in hereeature on link this is a fixed a bug and some stuff added some features";
 }
