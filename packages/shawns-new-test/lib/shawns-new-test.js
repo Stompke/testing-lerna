28e52cd3bab8974195be68165736eea7622e506e
Shawn Tompke <30378642+Stompke@users.noreply.github.com>
2022-11-18 15:59:47 -0600

    feat(shawns-new-test): added things and stuff (#44)
    * feat(shawns-new-test): removed comments.
    
    * fix(shawns-new-test): fixed and added stuff.
    
    Co-authored-by: GitHub Action <action@github.com>

--- packages/shawns-new-test/lib/shawns-new-test.js
+++ packages/shawns-new-test/lib/shawns-new-test.js
@@ -8,14 +8,10 @@ module.exports = shawnsNewTest;
 // comments
 // comments
 // comments
-// comments
-// comments
-// comments
-// comments
 
 
 
 
 function shawnsNewTest() {
-    return "Hello from noahshawnstmergecommitsestNewTest new stuff here";
+    return "Hello stuffrom noahshawnstmergecommitsestNewTest new stuff here";
 }
